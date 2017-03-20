229087936376712bc7993358c89f354b33005d45
ghostmania <ghostmania2@gmail.com>
2017-03-20 10:53:48 +0300

    added validation for empty input value

--- src/script.js
+++ src/script.js
@@ -3,25 +3,27 @@
  */
 $(function (){ // document ready start
 
-    $("#input").keypress(function(e){ // add li when press ENTER
-        if (e.keyCode == 13 ) {
-            add_element();
-        }
-    })
+        $("#input").keypress(function(e){ // add li when press ENTER
+            if (e.keyCode == 13 ) {
+                add_element();
+            }
+        })
 
-    $("button#add_btn").click( // add li when click ADD
+        $("button#add_btn").click( // add li when click ADD
             add_element
-    )
+        )
+
 
     function add_element() { // create li
-
-        var li = $("<li></li>").text($("#input").val()).addClass("Active"); // create li
-        li.prepend("<input type='checkbox' />");
-        li.append($("<button>x</button>").click(function (event) {
-            $(event.target).parent().detach(); // remove li
-        }));
-        $("#list").append(li); // insert created li to list
-        $("#input").val("");
+        if ($("#input")[0].value.trim() !== '') { // if input not empty
+            var li = $("<li></li>").text($("#input").val()).addClass("Active"); // create li
+            li.prepend("<input type='checkbox' />");
+            li.append($("<button>x</button>").click(function (event) {
+                $(event.target).parent().detach(); // remove li
+            }));
+            $("#list").append(li); // insert created li to list
+            $("#input").val("");
+        }
     }
 
 }) // document ready end
