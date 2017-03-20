496b8122c502a2d6d7233557c0b883f635ef0eb7
ghostmania <ghostmania2@gmail.com>
2017-03-20 11:59:17 +0300

    added function to checkbox to change status for li

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
  * Created by user on 17.03.17.
  */
 $(function (){ // document ready start
+        $("#input").focus(); // focus input when page is loaded
 
         $("#input").keypress(function(e){ // add li when press ENTER
             if (e.keyCode == 13 ) {
@@ -13,17 +14,21 @@ $(function (){ // document ready start
             add_element
         )
 
-
     function add_element() { // create li
         if ($("#input")[0].value.trim() !== '') { // if input not empty
-            var li = $("<li></li>").text($("#input").val()).addClass("Active"); // create li
-            li.prepend("<input type='checkbox' />");
+            var li = $("<li></li>").text($("#input").val()); // create li
+            li.prepend($("<input type='checkbox' />").click(function (event) { // function to sort elements by className
+                       $(this).parent().toggleClass("completed"); // when click on checkbox toggle class for li element
+                })
+            )
             li.append($("<button>x</button>").click(function (event) {
-                $(event.target).parent().detach(); // remove li
+                $(event.target).parent().detach(); // remove li when click remove btn
             }));
             $("#list").append(li); // insert created li to list
             $("#input").val("");
         }
     }
+
+
 
 }) // document ready end
