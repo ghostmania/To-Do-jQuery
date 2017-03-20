63dd48e8b383f43e9049006c6b15a8d4d3e8ecf7
ghostmania <ghostmania2@gmail.com>
2017-03-20 12:40:52 +0300

    added functions to buttons for different statuses

--- src/script.js
+++ src/script.js
@@ -14,10 +14,23 @@ $(function (){ // document ready start
             add_element
         )
 
+        $("#all_elements").click(
+            show_all
+        )
+
+        $("#active_elements").click(
+            show_active
+        )
+
+        $("#completed_elements").click(
+            show_completed
+        )
+
+
     function add_element() { // create li
         if ($("#input")[0].value.trim() !== '') { // if input not empty
             var li = $("<li></li>").text($("#input").val()); // create li
-            li.prepend($("<input type='checkbox' />").click(function (event) { // function to sort elements by className
+            li.prepend($("<input type='checkbox' />").click(function () { // function to sort elements by className
                        $(this).parent().toggleClass("completed"); // when click on checkbox toggle class for li element
                 })
             )
@@ -29,6 +42,18 @@ $(function (){ // document ready start
         }
     }
 
+    function show_all() {
+        $("li").css("display", "");
+    }
 
+    function show_active() {
+        $("li").css("display", "");
+        $(".completed").css("display", "none");
+    }
+
+    function show_completed() {
+        $("li").css("display", "none");
+        $(".completed").css("display", "");
+    }
 
 }) // document ready end
